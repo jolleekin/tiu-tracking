418687bef0c31b23c3e93901950ee849c440459a
LeeDangDung@gmail.com <LeeDangDung@gmail.com@744e66de-e35a-8734-4fd7-3b39aaf13268>
2011-04-02 21:11:22 +0000

    commented code in waiting (to be tested with database)

--- FingerPrintLocator/java/FingerPrintAlgorithm/src/FingerPrintLocator/FingerPrint.java
+++ FingerPrintLocator/java/FingerPrintAlgorithm/src/FingerPrintLocator/FingerPrint.java
@@ -1,45 +1,67 @@
+
 package FingerPrintLocator;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Enumeration;
 import java.util.Hashtable;
 
+/**
+ * @author Le Dang Dung
+ * FingerPrint class, implements FingerPrint engine: matching tag 
+ * location to sets of calibrated locations. Data used in calibrating are 
+ * RSSIs (received signal strength indicator) from a tag placed inside the 
+ * location. These signals measured by the surrounding detector nodes.
+ */
+
 public class FingerPrint 
-{
+{	
+	/**
+	 * ID of this FingerPrint pattern, associated with the tag that used for calibration 
+	 */
 	public int                                               fingerPrintID;
+	
+	/**
+	 * FingerPrint table, ArrayList of Block patterns (Transaction type)
+	 */
 	private ArrayList<Transaction>                           fingerPrintTable;
+	
 	private boolean                                          dirty;
 	
-	private Hashtable<Integer,Hashtable<Integer,Integer>>    statTable;	// this table holds stats mode values of the fingerPrintTable
-                                                                        // first key(blockID) index to a block, second key(detectorID)
-	                                                                    // indexes to the rssi value (stats) 
+	/**
+	 * This table holds stats mode values of the fingerPrintTable.
+	 * First key(blockID) index to a block, second key(detectorID)
+	 * indexes to the rssi value (stats).
+	 */
+	private Hashtable<Integer,Hashtable<Integer,Integer>>    statTable;	 
+	
+	/** 
+	 * current stats mode used in locating engine
+	 */
 	private String                                           statmode;
 	
-	public FingerPrint()
-	{
-		//fingerPrintTable = new ArrayList<Transaction>();
-		this.statmode = "mean";
-		this.dirty    = false;
-		this.statTable = new Hashtable<Integer,Hashtable<Integer,Integer>>();
-		
-	}
-	/*
-	 * create class instance with fingerPrintTable passed in
+	/**
+	 * Create class instance with fingerPrintTable passed in
+	 * This must be done before calling the locate() method.  
+	 * @param table: ArrayList of Transactions
 	 */
-	public FingerPrint(ArrayList<Transaction> fp)
+	public FingerPrint(ArrayList<Transaction> table)
 	{
 		this.statmode = "mean";
 		this.dirty    = false;
-		fingerPrintTable = fp;
-	 
+		fingerPrintTable = table;
 		this.fill_stat(this.statmode);
 	}
 	
-	/*
-	 * locating engine, receive a transaction with location unknown
-	 * - input : transaction t with unknown location, this.statTable
-	 * - return true if success (transaction modified with known location)
-	 * 
+	/**
+	 * Locating engine, receives a transaction with unknown location. 
+	 * The engine calculates Eucledean distance between current RSSI set
+	 * and the calibrated RSSI sets (matching pattern).
+	 * After calculation, the transaction will be modified with known location. 
+	 *
+	 * @param t - transaction with unknown location
+	 * @param mode - choose the stats mode for processing raw data
+	 *               : "mean", "median", "mode"
+	 * @return true if success, false otherwise
 	 */
 	public boolean locate(Transaction t, String mode)	
 	{
@@ -50,7 +72,7 @@ public class FingerPrint
 		}
 		
 		Hashtable<Double,Integer> ED_list = new Hashtable<Double,Integer>(); // list of Eucledean distance
-		ArrayList<Double> ED_mirror = new ArrayList<Double>();
+		ArrayList<Double> ED_mirror = new ArrayList<Double>(); // mirror list of the EU_list
 		Enumeration<Integer> blockIDs = this.statTable.keys(); // get block id list
 		while(blockIDs.hasMoreElements())
 		{
@@ -65,45 +87,58 @@ public class FingerPrint
 					eu_sum = (eu_sum + Math.pow(
 							(this.statTable.get(currentBlockID)).get(currentDetID)
 							- t.rssiLists.get(currentDetID).get(0)
-							, 2));
+							, 2));                            // do the sum first
 				}
 			}	
-			eu_sum = Math.sqrt(eu_sum);
-			ED_list.put(eu_sum,currentBlockID);    // put eucledean results on the list
-			ED_mirror.add(eu_sum);
+			eu_sum = Math.sqrt(eu_sum);                       // take sqrt() of sum
+			ED_list.put(eu_sum,currentBlockID);               // put eucledean results on the list
+			ED_mirror.add(eu_sum);                            // put sum in mirror list for sorting
 		}
-		Collections.sort(ED_mirror);               // sort list to find the min value
-		t.blockID = ED_list.get(ED_mirror.get(0)); // get the first element after sort, this will be the key to hash table
-		t.x = this.fingerPrintTable.get(t.blockID).x;
+		Collections.sort(ED_mirror);                          // sort list to find the min value
+		                                                      // ascending order, according to the natural ordering of its elements
+		t.blockID = ED_list.get(ED_mirror.get(0));            // get the first element after sort, this will be the key to hash table
+		t.x = this.fingerPrintTable.get(t.blockID).x;         // map to its x and y coordinates
 		t.y = this.fingerPrintTable.get(t.blockID).y;
 		return true;
 		
 		//return false;
 	}
+	
+	/**
+	 * commit new data to FingerPrint table
+	 * @return
+	 */
 	public ArrayList<Transaction> commit()
 	{
 		return null;
 	}
+	/**
+	 * tells whether data in FingerPrint table are changed
+	 * @return
+	 */
 	public boolean isDirty()
 	{
 		return this.dirty;
 	}
+	/**
+	 * Fill the internal statsTable with stats value processed from raw data table
+	 * @param mode - stats mode : "mean", "median", "mode"
+	 */
 	private void fill_stat(String mode)
 	{
 		this.statTable = new Hashtable<Integer, Hashtable<Integer,Integer>>();
-		for(Transaction block : this.fingerPrintTable) // the table is a list of blocks
+		for(Transaction block : this.fingerPrintTable)                            // the table is a list of blocks
 		{
-			this.statTable.put(block.blockID, new Hashtable<Integer, Integer>()); // create block pattern
-			Enumeration<Integer> mykeys = block.rssiLists.keys(); // mykeys = detector id list
+			this.statTable.put(block.blockID, new Hashtable<Integer, Integer>()); // create block pattern, with ID from input Block
+			Enumeration<Integer> mykeys = block.rssiLists.keys();                 // mykeys = detector id list
 			while(mykeys.hasMoreElements())
 			{
-				int current_key = mykeys.nextElement();  // current_key ; detectorID
+				int current_key = mykeys.nextElement();                           // current_key ; detectorID
 				ArrayList<Integer> current_list = (ArrayList<Integer>)block.rssiLists.get(current_key);
 				if(!current_list.isEmpty())
 				{
 					if (mode.equals("mean"))
 					{
-						//(this.statTable.get(t.hashCode())).put(current_key, Statistics.mean(current_list));
 						this.statTable.get(block.blockID).put(current_key, Statistics.mean(current_list));
 					}
 					else if(mode.equals("mode"))
--- FingerPrintLocator/java/FingerPrintAlgorithm/src/FingerPrintLocator/Transaction.java
+++ FingerPrintLocator/java/FingerPrintAlgorithm/src/FingerPrintLocator/Transaction.java
@@ -3,20 +3,59 @@ import java.util.ArrayList;
 import java.util.Hashtable;
 
 
-public class Transaction {
+public class Transaction 
+{
+	
+	/**
+	 * ID of calibrated block
+	 */
 	public int       blockID;
+	
+	/**
+	 * ID of calibrated/transmitting tag
+	 */
 	public int       tagID;
+	
+	/**
+	 * ID of incoming message
+	 */
 	public int       msgID;
+	
+	/**
+	 * x coordinate - Location of block, known when calibrating, unknown during locating 
+	 */
 	public float     x;
+	
+	/**
+	 * y coordinate - Location of block, known when calibrating, unknown during locating 
+	 */
 	public float     y;
+	
+	/**
+	 * battery level of tag 
+	 */
 	public int       batteryLevel;
+	
+	/**
+	 * timestamp when proxy receives this transaction 
+	 */
 	public long      time;
+	
+	/**
+	 * RSSI data (fingerprint pattern), keys: detectorID, value: list of RSSI (raw) 
+	 */
 	public Hashtable<Integer, ArrayList<Integer>> rssiLists;// keys: detectorIDs
-	//public ArrayList<ArrayList<Integer>> rssiList;
+	
+	
 	public Transaction()
 	{
-		rssiLists = new Hashtable<Integer, ArrayList<Integer>>();
-	
+		this.rssiLists = new Hashtable<Integer, ArrayList<Integer>>();
+		this.blockID = -1;
+		this.msgID = -1;
+		this.tagID = -1;
+		this.time = -1;
+		this.x = -1;
+		this.y = -1;
 	}
 	
 }
