3319fa8287a46404632bd3c60d6de6c4676dd816
jolleekin@gmail.com <jolleekin@gmail.com@744e66de-e35a-8734-4fd7-3b39aaf13268>
2011-04-03 22:39:26 +0000

    Refined LocationEngine and Tag classes.

--- TIUTracking/src/edu/pdx/capstone/tiutracking/LocationEngine.java
+++ TIUTracking/src/edu/pdx/capstone/tiutracking/LocationEngine.java
@@ -1,37 +1,52 @@
 package edu.pdx.capstone.tiutracking;
 
+import java.util.ArrayList;
 import java.util.Hashtable;
 
+/**
+ * A base class for all location engines. Any class derived from this class must
+ * override the constructor and all the abstract methods.
+ * 
+ * @author Kin
+ * 
+ */
 public abstract class LocationEngine {
-	
+
 	/**
-	 * Abstract constructor. Any class derived from this class must
-	 * override this constructor.
+	 * Creates an instance of LocationEngine class. The engine uses the provided
+	 * raw calibration data and detectors' locations to "learn" how to locate
+	 * the tags.
 	 * 
-	 * @param detectors		A table containing the locations of all detectors.
+	 * @param rawData
+	 *            The data collected during calibration.
+	 * @param detectors
+	 *            A table containing the locations of all detectors.
 	 */
-	public LocationEngine(/* raw data goes here, */Hashtable<Integer, Vector2D> detectors) {
-		
+	public LocationEngine(ArrayList<Tag> rawData,
+			Hashtable<Integer, Vector2D> detectors) {
+
 	}
-	
+
 	/**
-	 *	Locate an asset tag based on measured RSSI values.
-	 *
-	 *	@param	tag		The tag whose location is to be determined.
+	 * Locates an asset tag based on measured RSSI values.
+	 * 
+	 * @param tag
+	 *            The tag whose location is to be determined.
 	 */
 	public abstract void locate(Tag tag);
 
 	/**
-	 * Retrieve a configuration table for this engine.
+	 * Retrieves a configuration table for this engine.
 	 * 
-	 * @return	A hash table which contains the engine's configuration.
+	 * @return A hash table which contains the engine's configuration.
 	 */
 	public abstract Hashtable<String, String> getConfiguration();
-	
+
 	/**
-	 * Apply configuration to the engine.
+	 * Applies configuration to the engine.
 	 * 
-	 * @param config	A hash table which contains the engine's configuration.
+	 * @param config
+	 *            A hash table which contains the engine's configuration.
 	 */
 	public abstract void setConfiguration(Hashtable<String, String> config);
 }
