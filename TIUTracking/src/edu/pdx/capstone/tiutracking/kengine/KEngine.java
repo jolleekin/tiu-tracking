dbe81c3ba0529103923c35dc252334e25983e597
jolleekin@gmail.com <jolleekin@gmail.com@744e66de-e35a-8734-4fd7-3b39aaf13268>
2011-04-04 14:44:52 +0000

    Changed LocationEngine interface.

--- TIUTracking/src/edu/pdx/capstone/tiutracking/kengine/KEngine.java
+++ TIUTracking/src/edu/pdx/capstone/tiutracking/kengine/KEngine.java
@@ -3,34 +3,34 @@ package edu.pdx.capstone.tiutracking.kengine;
 import java.util.ArrayList;
 import java.util.Hashtable;
 
-import edu.pdx.capstone.tiutracking.DataPacket;
 import edu.pdx.capstone.tiutracking.LocationEngine;
-import edu.pdx.capstone.tiutracking.Vector2D;
+import edu.pdx.capstone.tiutracking.shared.DataPacket;
+import edu.pdx.capstone.tiutracking.shared.Vector2D;
 
-public class KEngine extends LocationEngine {
+public class KEngine implements LocationEngine {
 
-	public KEngine(ArrayList<DataPacket> rawData,
+	@Override
+	public void learn(ArrayList<DataPacket> rawData,
 			Hashtable<Integer, Vector2D> detectors) {
-		super(rawData, detectors);
+		// TODO Auto-generated method stub
 
-		
 	}
 
 	@Override
 	public void locate(DataPacket dataPacket) {
-
+		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public Hashtable<String, String> getConfiguration() {
-
+		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public void setConfiguration(Hashtable<String, String> config) {
-
+		// TODO Auto-generated method stub
 
 	}
 
