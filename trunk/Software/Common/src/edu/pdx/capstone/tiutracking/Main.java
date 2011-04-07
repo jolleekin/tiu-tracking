ac914a0dc1100fc8f2546f1e2df9a85311e7fc6a
jolleekin@gmail.com <jolleekin@gmail.com@744e66de-e35a-8734-4fd7-3b39aaf13268>
2011-04-07 20:13:22 +0000

    Added Boolean type and range checking to ConfigurationParam.

--- trunk/Software/Common/src/edu/pdx/capstone/tiutracking/Main.java
+++ trunk/Software/Common/src/edu/pdx/capstone/tiutracking/Main.java
@@ -10,18 +10,25 @@ public class Main {
 	public static void main(String[] args) {
 
 		ArrayList<ConfigurationParam> params = new ArrayList<ConfigurationParam>();
-		params.add(new ConfigurationParam("A", null, 0.0));
-		params.add(new ConfigurationParam("B", null, StatisticMode.MAX));
+		params.add(new ConfigurationParam("A", null, 0.1, 0.0, 1.0));
+		params.add(new ConfigurationParam("B", null, StatisticMode.MAX, null, null));
+		params.add(new ConfigurationParam("C", null, false, null, null));
 
 		for (ConfigurationParam p : params) {
-			String[] list = p.getValueList();
+			String[] list = p.getValueArray();
 			if (list != null) {
 				for (String item : list)
 					System.out.println(item);
 			} else {
 				System.out.println("Not an enum");
 			}
+			System.out.println("-----------------");
 		}
+		ConfigurationParam p = new ConfigurationParam("Holly", "", 5, 0, 10);
+		System.out.println(p.getValue() + " " + p.minValue + " " + p.maxValue);
+		p.setValue("3");
+		System.out.println(p.getValue() + " " + p.minValue + " " + p.maxValue);
+		p.setValue("-8");
 	}
 
 }
