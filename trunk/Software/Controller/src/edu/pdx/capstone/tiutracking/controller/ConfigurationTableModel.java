f8b542a4831f9160ebcf23cf3426bde5e7bd1527
danieljayferguson@gmail.com <danieljayferguson@gmail.com@744e66de-e35a-8734-4fd7-3b39aaf13268>
2011-04-09 22:22:04 +0000

    Controller - added ConfigurationTableModel class.

---
+++ trunk/Software/Controller/src/edu/pdx/capstone/tiutracking/controller/ConfigurationTableModel.java
@@ -0,0 +1,50 @@
+package edu.pdx.capstone.tiutracking.controller;
+import java.util.ArrayList;
+
+import edu.pdx.capstone.tiutracking.common.*;
+import javax.swing.table.AbstractTableModel;
+
+public class ConfigurationTableModel extends AbstractTableModel{
+
+	public String[] columnNames ={"Name", "Description", "Value"};
+	
+	ArrayList<ConfigurationParam> configParams = null;
+
+	public ConfigurationTableModel(){
+		
+	}
+	public void setData(ArrayList<ConfigurationParam> params){
+		configParams = params;
+	}
+	
+	@Override
+	public int getColumnCount() {
+		return 3;
+	}
+	
+	public String getColumnName(int col){
+		return columnNames[col];
+	}
+
+	@Override
+	public int getRowCount() {
+		// TODO Auto-generated method stub
+		return configParams.size();
+	}
+
+	@Override
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		// TODO Auto-generated method stub
+		switch (columnIndex){
+		case 0:
+			return configParams.get(rowIndex).getTypeName();
+		case 1:
+			return configParams.get(rowIndex).description;
+		case 2:
+			return configParams.get(rowIndex).getValue();
+				
+		}
+		return null;
+	}
+
+}
